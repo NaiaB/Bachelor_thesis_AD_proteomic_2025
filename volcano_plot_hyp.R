fad289c0831907ed6a1510c07942e04d7ea915d9
Naia Barney <n.barneymachado@student.maastrichtuniversity.nl>
2025-06-27 22:05:05 +0200

    volcano_plot_hyp.R

--- volcano_plot_hyp.R
+++ volcano_plot_hyp.R
@@ -1,5 +1,5 @@
 
-#CN A+ Hyp- vs CN A-
+##CN A+ Hyp- vs CN A-
 
 dev.off()
 with(resultsproteomics_hyp, plot(diff_CN_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_CN_A_Yes_Hyp_No_vs_CN_A_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-1.1,1.1), ylim=c(0, 5)))
@@ -12,7 +12,7 @@ library(calibrate)
 with(subset(resultsproteomics_hyp, p_CN_A_Yes_Hyp_No_vs_CN_A_No<.00475), points(diff_CN_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_CN_A_Yes_Hyp_No_vs_CN_A_No), pch=20, col="dodgerblue3"))
 with(subset(resultsproteomics_hyp, p_CN_A_Yes_Hyp_No_vs_CN_A_No<.00475), textxy(diff_CN_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_CN_A_Yes_Hyp_No_vs_CN_A_No), labs=Protein, cex=.4))
 
-#CN A+ Hyp+ vs CN A-
+##CN A+ Hyp+ vs CN A-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_CN_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_CN_A_Yes_Hyp_Yes_vs_CN_A_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-1.7,1.3), ylim=c(0, 5)))
 
@@ -24,7 +24,7 @@ library(calibrate)
 with(subset(resultsproteomics_hyp, p_CN_A_Yes_Hyp_Yes_vs_CN_A_No<.01429), points(diff_CN_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_CN_A_Yes_Hyp_Yes_vs_CN_A_No), pch=20, col="dodgerblue3"))
 with(subset(resultsproteomics_hyp, p_CN_A_Yes_Hyp_Yes_vs_CN_A_No<.01429), textxy(diff_CN_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_CN_A_Yes_Hyp_Yes_vs_CN_A_No), labs=Protein, cex=.4))
 
-#MCI A+ Hyp- vs CN A-
+##MCI A+ Hyp- vs CN A-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_MCI_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_MCI_A_Yes_Hyp_No_vs_CN_A_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-0.8,-0.6), ylim=c(4.2, 4.6)))
 
@@ -37,7 +37,7 @@ with(subset(resultsproteomics_hyp, p_MCI_A_Yes_Hyp_No_vs_CN_A_No<.00005), points
 with(subset(resultsproteomics_hyp, p_MCI_A_Yes_Hyp_No_vs_CN_A_No<.00005), textxy(diff_MCI_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_MCI_A_Yes_Hyp_No_vs_CN_A_No), labs=Protein, cex=.4))
 
 
-#MCI A+ Hyp+ vs CN A-
+##MCI A+ Hyp+ vs CN A-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_MCI_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_MCI_A_Yes_Hyp_Yes_vs_CN_A_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-1.4,1.4), ylim=c(0, 5)))
 
@@ -49,7 +49,7 @@ library(calibrate)
 with(subset(resultsproteomics_hyp, p_MCI_A_Yes_Hyp_Yes_vs_CN_A_No<.00096), points(diff_MCI_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_MCI_A_Yes_Hyp_Yes_vs_CN_A_No), pch=20, col="dodgerblue3"))
 with(subset(resultsproteomics_hyp, p_MCI_A_Yes_Hyp_Yes_vs_CN_A_No<.00096), textxy(diff_MCI_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_MCI_A_Yes_Hyp_Yes_vs_CN_A_No), labs=Protein, cex=.4))
 
-#AD A+ Hyp- vs CN A-
+##AD A+ Hyp- vs CN A-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_AD_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_AD_A_Yes_Hyp_No_vs_CN_A_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-3.05,3.05), ylim=c(0, 5)))
 
@@ -61,7 +61,7 @@ library(calibrate)
 with(subset(resultsproteomics_hyp, p_AD_A_Yes_Hyp_No_vs_CN_A_No<.01017), points(diff_AD_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_AD_A_Yes_Hyp_No_vs_CN_A_No), pch=20, col="dodgerblue3"))
 with(subset(resultsproteomics_hyp, p_AD_A_Yes_Hyp_No_vs_CN_A_No<.01017), textxy(diff_AD_A_Yes_Hyp_No_vs_CN_A_No, -log10(p_AD_A_Yes_Hyp_No_vs_CN_A_No), labs=Protein, cex=.4))
 
-#AD A+ Hyp+ vs CN A-
+##AD A+ Hyp+ vs CN A-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_AD_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_AD_A_Yes_Hyp_Yes_vs_CN_A_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-1.8,1.8), ylim=c(0, 4)))
 
@@ -73,7 +73,7 @@ library(calibrate)
 with(subset(resultsproteomics_hyp, p_AD_A_Yes_Hyp_Yes_vs_CN_A_No<.00758), points(diff_AD_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_AD_A_Yes_Hyp_Yes_vs_CN_A_No), pch=20, col="dodgerblue3"))
 with(subset(resultsproteomics_hyp, p_AD_A_Yes_Hyp_Yes_vs_CN_A_No<.00758), textxy(diff_AD_A_Yes_Hyp_Yes_vs_CN_A_No, -log10(p_AD_A_Yes_Hyp_Yes_vs_CN_A_No), labs=Protein, cex=.4))
 
-#CN A+ Hyp+ vs CN A+ Hyp-
+##CN A+ Hyp+ vs CN A+ Hyp-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No, -log10(p_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-1.8,1.8), ylim=c(0, 4)))
 
@@ -85,7 +85,7 @@ library(calibrate)
 with(subset(resultsproteomics_hyp, p_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No<.02686), points(diff_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No, -log10(p_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No), pch=20, col="dodgerblue3"))
 with(subset(resultsproteomics_hyp, p_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No<.02686), textxy(diff_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No, -log10(p_CN_A_Yes_Hyp_Yes_vs_CN_A_Yes_Hyp_No), labs=Protein, cex=.4))
 
-#MCI A+ Hyp+ vs MCI A+ Hyp-
+##MCI A+ Hyp+ vs MCI A+ Hyp-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No, -log10(p_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-1.6,1.6), ylim=c(0, 4.5)))
 
@@ -97,7 +97,7 @@ library(calibrate)
 with(subset(resultsproteomics_hyp, p_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No<.01545), points(diff_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No, -log10(p_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No), pch=20, col="dodgerblue3"))
 with(subset(resultsproteomics_hyp, p_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No<.01545), textxy(diff_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No, -log10(p_MCI_A_Yes_Hyp_Yes_vs_MCI_A_Yes_Hyp_No), labs=Protein, cex=.4))
 
-#AD A+ Hyp+ vs AD A+ Hyp-
+##AD A+ Hyp+ vs AD A+ Hyp-
 dev.off()
 with(resultsproteomics_hyp, plot(diff_AD_A_Yes_Hyp_Yes_vs_AD_A_Yes_Hyp_No, -log10(p_AD_A_Yes_Hyp_Yes_vs_AD_A_Yes_Hyp_No), pch=20, col="grey41", main="Volcano plot", xlim=c(-2.9,2.9), ylim=c(0, 4)))
 
